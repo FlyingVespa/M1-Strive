0fe359a851f6308919dae757f3d73096fc159015
Helena Nel <mailhedri@gmail.com>
2021-03-15 16:50:05 +0100

    thrid commit

--- M1D6/main.js
+++ M1D6/main.js
@@ -1,8 +1,22 @@
 //change h1 color
-document.querySelector("h1").style.color = "#000066";
+document.getElementById("shop-name").style.color = "#000066";
 document.querySelector("h1").innerHTML = " Panarotti's Restuarant "
 
 
-let changeBackground = function(btn) {
-    document.body.style.backgroundColor = 'green'
-}
+let changeBackground = function() {
+    document.body.style.backgroundColor = 'green';
+};
+
+let changeAddress = function() {
+    document.getElementById("address").innerHTML =  "21 New Amsterdam ListeningStateChangedEvent, Old Square, Little ROck, USA"
+  }
+
+
+function toggleBtnClick() {
+    let imgToggle = document.getElementById("foodimg");
+    if (imgToggle.style.display === "none") {
+        imgToggle.style.display = "block";
+    } else {
+        imgToggle.style.display = "none";
+    }
+};
