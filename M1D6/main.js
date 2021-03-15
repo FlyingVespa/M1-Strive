8deaef9070e8f585065efe7f04c9e734f5176804
Helena Nel <mailhedri@gmail.com>
2021-03-15 16:05:21 +0100

    second submit

---
+++ M1D6/main.js
@@ -0,0 +1,8 @@
+//change h1 color
+document.querySelector("h1").style.color = "#000066";
+document.querySelector("h1").innerHTML = " Panarotti's Restuarant "
+
+
+let changeBackground = function(btn) {
+    document.body.style.backgroundColor = 'green'
+}
