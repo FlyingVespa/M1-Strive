c08b52cb3355c910ba7b3a8de88831163ee4e021
Helena Nel <mailhedri@gmail.com>
2021-03-16 17:58:08 +0100

    d7 completed
    conflict naming convetion (header )
    changed to link selector

--- M1D7/main.js
+++ M1D7/main.js
@@ -31,13 +31,14 @@ const changePcontent = function () {
     //
 };
 
-/* EXERCISE 4
+/* EXERCISE 4  ---------------------------- WORKS
 Write a function for changing the destination of every link to https://www.google.com
 */
 
 const changeUrls = function () {
     //
-    document.getElementsByTagName("a").setAttribute("href", "http://www.google.com");
+    document.querySelector("a").setAttribute("href", "http://www.google.com");
+    console.log("link has been changed");
 };
 
 /* EXERCISE 5 ---------------------------- WORKS
@@ -57,7 +58,12 @@ Write a function for adding a second paragraph to the div.
 
 const addParagraph = function (content) {
     let newP = document.createElement("p")
-    newP.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elitTotam accusamus itaque cumque dolore dolor repudiandae commodi";
+    newP.innerHTML = `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat 
+    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    `
     document.getElementsByTagName("div")[0].appendChild(newP);
 
 };
@@ -83,24 +89,19 @@ const paintItGreen = function () {
 
 };
 
-/* EXERCISE 9 
+/* EXERCISE 9   ---------------------------- WORKS
 Make the heading of the page change color every time the user clicks on it.
 */
 
 const makeItClickable = function () {
-    let colors = ['red', 'blue', 'green'];
-    let header = document.querySelector("header").children;
-    for (i = 0; i < header.length; i++) {
+    let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+    let headers = document.querySelector("header").children;
+    for (i = 0; i < headers.length; i++) {
         // Pick a random color from the array 'colors'.
-        let header = header[i].style;
+        let header = headers[i].style;
         let randomColorIndex = Math.floor(Math.random() * colors.length);
-
         header.color = colors[randomColorIndex];
-        header.width = '100px';
-        header.height = '100px';
-        header.display = 'inline-block';
     }
-    //
 };
 
 /* EXERCISE 10  ---------------------------- WORKS
@@ -113,12 +114,16 @@ const changeFooterText = function () {
     //
 };
 
-/* EXERCISE 11
+/* EXERCISE 11  ---------------------------- WORKS
 Attach an event listener to the input field in the page for console logging its value just after any keystroke.
 */
 
 const inputField = document.getElementById("input-field");
 // ...
+
+inputField.addEventListener('input-field', (e) => {
+    result.textContent = "typed text:" + e.data
+});
 
 /* EXERCISE 12  ---------------------------- WORKS
 Create a new welcome alert message when the page successfully loads.
